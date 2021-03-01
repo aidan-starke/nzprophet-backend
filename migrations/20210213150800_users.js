891a0f96049e3f5687f9d570e3541067c964ef16
Aidan Starke <starke.aidan@gmail.com>
2021-03-01 17:38:34 +1300

    fix migrations

--- migrations/20210213150800_users.js
+++ migrations/20210213150800_users.js
@@ -1,10 +1,10 @@
 
 exports.up = function (knex) {
     return knex.schema.createTable('users', (table) => {
-        table.increments('id')
+        table.increments('id').primary()
         table.string('name')
-        table.string('username')
-        table.string('email')
+        table.string('username').unique()
+        table.string('email').unique()
         table.string('password_hash')
         table.timestamp('reset_expires_at')
         table.string('reset_password_key')
