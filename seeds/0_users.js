d8bed44615eee245ec487b8a4cb1e747a5010c03
Aidan Starke <starke.aidan@gmail.com>
2021-03-26 15:34:07 +1300

    remove id

--- seeds/0_users.js
+++ seeds/0_users.js
@@ -1,7 +1,7 @@
 
 exports.seed = function (knex) {
   return knex('users').insert([
-    { id: 1, name: 'Aidan', username: 'el coacho', email: 'starke.aidan@gmail.com', password_hash: '', reset_password_key: '' },
-    { id: 2, name: 'Bradley', username: 'muthaphukknge', email: '19bradley97@gmail.com', password_hash: '', reset_password_key: '' }
+    { name: 'Aidan', username: 'el coacho', email: 'starke.aidan@gmail.com', password_hash: '', reset_password_key: '' },
+    { name: 'Bradley', username: 'muthaphukknge', email: '19bradley97@gmail.com', password_hash: '', reset_password_key: '' }
   ]);
 };
